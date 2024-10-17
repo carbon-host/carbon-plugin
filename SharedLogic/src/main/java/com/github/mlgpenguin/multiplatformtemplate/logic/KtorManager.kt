a8f55870b045a8731e587895d71793d7a6a7b133
Stephen <138625055+stephendotgg@users.noreply.github.com>
2024-10-17 02:39:19 +0100

    Ktor shared logic

--- SharedLogic/src/main/java/com/github/mlgpenguin/multiplatformtemplate/logic/KtorManager.kt
+++ SharedLogic/src/main/java/com/github/mlgpenguin/multiplatformtemplate/logic/KtorManager.kt
@@ -1,13 +1,35 @@
 package com.github.mlgpenguin.multiplatformtemplate.logic
 
-class KtorManager {
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+object KtorManager {
 
     fun startServer(proxy: Boolean) {
-        if (proxy) {
-            // TODO: Implement proxy server
-        } else {
-            // TODO: Implement non-proxy server
-        }
+        embeddedServer(
+            Netty,
+            port = 8080,
+            module = if (proxy) Application::proxyModule else Application::nonProxyModule
+        ).start(wait = true)
     }
 
+}
+
+fun Application.proxyModule() {
+    routing {
+        get("/") {
+            call.respondText("Proxy")
+        }
+    }
+}
+
+fun Application.nonProxyModule() {
+    routing {
+        get("/") {
+            call.respondText("Non proxy")
+        }
+    }
 }
