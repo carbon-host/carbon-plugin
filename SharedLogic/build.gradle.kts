945e8e8042fb30174ee0d1e854ee6344e84b11f3
Stephen <138625055+stephendotgg@users.noreply.github.com>
2024-10-17 02:46:58 +0100

    Ktor shared logic

--- SharedLogic/build.gradle.kts
+++ SharedLogic/build.gradle.kts
@@ -14,8 +14,10 @@ dependencies {
     // IF the downstream modules (spigot/velocity/bungee) need access to KTOR dependency,
     // change 'implementation' to 'api' otherwise abstract methods.
 
-    dependencies {
-        implementation("io.ktor:ktor-server-core:3.0.0")
-        implementation("io.ktor:ktor-server-netty:3.0.0")
-    }
+    implementation("io.ktor:ktor-server-core:3.0.0")
+    implementation("io.ktor:ktor-server-netty:3.0.0")
+}
+
+application {
+    mainClass.set("com.github.mlgpenguin.multiplatformtemplate.logic.KtorManager")
 }
--- SharedLogic/src/main/java/com/github/mlgpenguin/multiplatformtemplate/logic/KtorManager.kt
+++ SharedLogic/src/main/java/com/github/mlgpenguin/multiplatformtemplate/logic/KtorManager.kt
@@ -6,7 +6,7 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-object KtorManager {
+class KtorManager {
 
     fun startServer(proxy: Boolean) {
         embeddedServer(
