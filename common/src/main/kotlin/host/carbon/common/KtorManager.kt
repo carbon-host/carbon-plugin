6210a89f34942a63c7fbbbf6ffff51ae34b1ecfe
Dawson <hello@dawson.gg>
2025-01-20 17:19:01 -0500

    feat: Customize port via config.yml

--- common/src/main/kotlin/host/carbon/common/KtorManager.kt
+++ common/src/main/kotlin/host/carbon/common/KtorManager.kt
@@ -1,88 +1,97 @@
 package host.carbon.common
 
-import host.carbon.common.types.*
-import host.carbon.common.types.players.PlayerCountInfo
+import host.carbon.common.types.PaginatedResponse
+import host.carbon.common.types.Pagination
+import host.carbon.common.types.ServerInfo
+import host.carbon.common.types.ServerResourceInfo
 import host.carbon.common.types.players.PlayersInfo
 import io.ktor.serialization.gson.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-class KtorManager(private val carbonAPI: CarbonAPI) {
+
+class KtorManager(private val carbonAPI: CarbonAPI, private val port: Int) {
+
+    private val server = embeddedServer(
+        Netty,
+        port = port,
+    ) {
+        install(CORS)
+        install(ContentNegotiation) {
+            gson {
+                setPrettyPrinting()
+            }
+        }
+
+        routing {
+            get("/") {
+                call.respondText("Hello World!")
+            }
+
+            route("/v1") {
+                get {
+                    val info = ServerInfo(
+                        carbonAPI.getTPS(),
+                        carbonAPI.getMSPT(),
+                        ServerResourceInfo(
+                            carbonAPI.getMemoryUsage(),
+                            carbonAPI.getTotalMemory(),
+                            carbonAPI.getCPUUsage(),
+                            carbonAPI.getCPUCores()
+                        ),
+                        carbonAPI.getPlayerCountInfo(),
+                    )
+
+                    call.respond(info)
+                }
+
+                get("/players") {
+                    val limit = call.request.queryParameters["limit"]?.toInt() ?: 25
+                    val offset = call.request.queryParameters["offset"]?.toInt() ?: 0
+
+                    val players = carbonAPI.getOnlinePlayers(limit.coerceAtMost(100), offset)
+
+                    call.respond(
+                        PaginatedResponse(
+                            Pagination(limit, offset),
+                            PlayersInfo(
+                                players,
+                                carbonAPI.getPlayerCountInfo()
+                            )
+                        )
+                    )
+                }
+
+                // TODO: Implement pagination
+                get("/commands") {
+                    val limit = call.request.queryParameters["limit"]?.toInt() ?: 250
+                    val offset = call.request.queryParameters["offset"]?.toInt() ?: 0
+                    val query = call.request.queryParameters["query"]
+
+                    val commands = carbonAPI.getCommands(query)
+
+                    call.respond(
+                        PaginatedResponse(
+                            Pagination(limit, offset),
+                            commands
+                        )
+                    )
+                }
+            }
+        }
+    }
 
     fun startServer() {
-        embeddedServer(
-            Netty,
-            port = 6,
-        ) {
-            install(CORS)
-            install(ContentNegotiation) {
-                gson {
-                    setPrettyPrinting()
-                }
-            }
+        server.start(wait = false)
+    }
 
-            routing {
-                get("/") {
-                    call.respondText("Hello World!")
-                }
-
-                route("/v1") {
-                    get {
-                        val info = ServerInfo(
-                            carbonAPI.getTPS(),
-                            carbonAPI.getMSPT(),
-                            ServerResourceInfo(
-                                carbonAPI.getMemoryUsage(),
-                                carbonAPI.getTotalMemory(),
-                                carbonAPI.getCPUUsage(),
-                                carbonAPI.getCPUCores()
-                            ),
-                            carbonAPI.getPlayerCountInfo(),
-                        )
-
-                        call.respond(info)
-                    }
-
-                    get("/players") {
-                        val limit = call.request.queryParameters["limit"]?.toInt() ?: 25
-                        val offset = call.request.queryParameters["offset"]?.toInt() ?: 0
-
-                        val players = carbonAPI.getOnlinePlayers(limit.coerceAtMost(100), offset)
-
-                        call.respond(
-                            PaginatedResponse(
-                                Pagination(limit, offset),
-                                PlayersInfo(
-                                    players,
-                                    carbonAPI.getPlayerCountInfo()
-                                )
-                            )
-                        )
-                    }
-
-                    // TODO: Implement pagination
-                    get("/commands") {
-                        val limit = call.request.queryParameters["limit"]?.toInt() ?: 250
-                        val offset = call.request.queryParameters["offset"]?.toInt() ?: 0
-                        val query = call.request.queryParameters["query"]
-
-                        val commands = carbonAPI.getCommands(query)
-
-                        call.respond(
-                            PaginatedResponse(
-                                Pagination(limit, offset),
-                                commands
-                            )
-                        )
-                    }
-                }
-            }
-        }.start(wait = false)
+    fun stopServer() {
+        server.stop()
     }
 
 }
--- spigot/src/main/kotlin/host/carbon/plugin/spigot/CarbonPlugin.kt
+++ spigot/src/main/kotlin/host/carbon/plugin/spigot/CarbonPlugin.kt
@@ -10,9 +10,23 @@ import org.bukkit.plugin.java.JavaPlugin
 
 
 class CarbonPlugin : JavaPlugin() {
+    private lateinit var ktorManager: KtorManager
+
     override fun onEnable() {
-        KtorManager(CarbonPluginAPI()).startServer()
+        saveConfig()
+
+        val port = config.getInt("port")
+
+        ktorManager = KtorManager(CarbonPluginAPI(), port)
+        ktorManager.startServer()
+
+        logger.info("Carbon API started on port $port")
     }
 
-    override fun onDisable() {}
+    override fun onDisable() {
+        if (::ktorManager.isInitialized) {
+            ktorManager.stopServer()
+            logger.info("Carbon API stopped")
+        }
+    }
 }
