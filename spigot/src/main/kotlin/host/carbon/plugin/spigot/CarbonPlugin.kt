26a40d566f0ccc6beb87ebf3e1dae41cd809cbec
stephendotgg <138625055+stephendotgg@users.noreply.github.com>
2025-02-19 23:26:14 +0000

    Make Spigot generate file if not exists

--- spigot/src/main/kotlin/host/carbon/plugin/spigot/CarbonPlugin.kt
+++ spigot/src/main/kotlin/host/carbon/plugin/spigot/CarbonPlugin.kt
@@ -1,15 +1,33 @@
 package host.carbon.plugin.spigot
 
 import host.carbon.common.KtorManager
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+import java.util.*
 
 class CarbonPlugin : JavaPlugin() {
     private lateinit var ktorManager: KtorManager
 
     override fun onEnable() {
-        saveConfig()
+        if (!dataFolder.exists()) {
+            dataFolder.mkdir()
+        }
 
-        val port = config.getInt("port")
+        var port = 25505
+        var carbonKey = UUID.randomUUID()
+
+        val configFile = File(dataFolder, "config.yml")
+        if (configFile.exists()) {
+            port = config.getInt("port")
+            carbonKey = UUID.fromString("carbon-key")
+        } else {
+            configFile.createNewFile()
+
+            config.set("port", 25505)
+            config.set("carbon-key", carbonKey)
+            saveConfig()
+        }
 
         ktorManager = KtorManager(CarbonPluginAPI(), port)
         ktorManager.startServer()
